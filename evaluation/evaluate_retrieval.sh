6e680bee34843f7cf786a6c22cc443100a9fd1cf
Martí Umbert <marti.umbert@bsc.es>
2025-02-11 17:16:51 +0100

    evaluate_retrieval.sh: reorganize code, use CONDA_ENV from CONFIG_BASH file

--- evaluation/evaluate_retrieval.sh
+++ evaluation/evaluate_retrieval.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # PARAMETERS
-SCRIPT_DIR=$(dirname $(realpath $0))
-CONFIG_FILE="config/test.yaml"
-ENV_NAME="eadop_rag"
+DELIM="##############################"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CONFIG_BASH="$SCRIPT_DIR/eadop_rag_evaluation.config"
+CONFIG_YAML="config/test.yaml"
+
+# LOAD CONFIG
+. $CONFIG_BASH
+echo -e "$DELIM\nLoading config in file $CONFIG_BASH\n" && cat $CONFIG_BASH && echo -e "\n$DELIM\n"
 
 # ACTIVATE CONDA ENVIRONMENT
 eval "$(conda shell.bash hook)"
-cmd="conda activate $ENV_NAME"
-echo $cmd && eval $cmd
+cmd="conda activate $CONDA_ENV"
+echo -e "\n$cmd\n" && eval $cmd
 
 # RUN SCRIPT
 cd $SCRIPT_DIR
-cmd="python evaluate_retrieval.py -c $CONFIG_FILE"
-echo $cmd && eval $cmd
+cmd="python -W ignore evaluate_retrieval.py -c $CONFIG_YAML"
+echo -e "\n$cmd\n" && eval $cmd
