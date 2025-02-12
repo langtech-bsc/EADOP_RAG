9c50d0ca8b39423e57785f4d6c2f8f4319ff48f0
Martí Umbert <marti.umbert@bsc.es>
2025-02-12 11:03:30 +0100

    evaluate_retrieval.py: bugfix setting vectorstore_repo_name to None if not used

--- evaluation/evaluate_retrieval.py
+++ evaluation/evaluate_retrieval.py
@@ -35,8 +35,12 @@ class EvaluateRetrieval():
         else:
 
             config["input"]["model_dir"] = None
+
         if not "vectorstore_dir" in config["input"]:
             config["input"]["vectorstore_dir"] = None
+
+        if not "vectorstore_repo_name" in self.config["input"]:
+            config["input"]["vectorstore_repo_name"] = None
 
         config["output"]["json_file"] = utils.prepare_json_filename_with_date(output_dir = config["output"]["dir"])
 
